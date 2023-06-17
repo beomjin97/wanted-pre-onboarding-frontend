719149c3ea37ad274928c174f1f2d70c0469b0a2
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-06-17 19:09:01 +0900

    add index path

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import TodoPage from "./pages/Todo";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/todo" element={<TodoPage />} />
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>원티드 과제</div>;
+};
+
+export default Home;
